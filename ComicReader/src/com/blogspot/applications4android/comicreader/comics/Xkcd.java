b301fd59d57560b148ae58e2603ad00fc1da68b3
umesh2802 <umesh2802@gmail.com>
2015-04-07 09:44:24 +0530

    Fix for Xkcd

--- ComicReader/src/com/blogspot/applications4android/comicreader/comics/Xkcd.java
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/Xkcd.java
@@ -57,36 +57,37 @@ public class Xkcd extends IndexedComic {
 	@Override
 	protected String parse(String url, BufferedReader reader, Strip strip)
 			throws IOException {
-    	String str;
-		String final_str = null;
-		String final_title = null;
-		String final_title1 = null;
-		String final_itext = null;
+    		String str;
+		String image_url = null;
+		String str_index = null;
+		String str_title = null;
+		String str_itext = null;
 		while ((str = reader.readLine()) != null) {
-			int index1 = str.indexOf("<img src=\"http://imgs.xkcd.com/comics");
+			int index1 = str.indexOf("http://imgs.xkcd.com/comics");
 			if (index1 != -1) {
-				final_str = str;
-				final_itext = str;
+				image_url = str.replaceAll(".*http:","http:");
 			}
+			int index2 = str.indexOf("src=\"//imgs.xkcd.com/comics");
+			if (index2 != -1) {
+				str_itext = str;
+			}
+
 			int index3 = str.indexOf("Permanent link to this comic");
 			if (index3 != -1) {
-				final_title = str;
+				str_index = str;
 			}
 			if(str.indexOf("<title>") != -1) {
-				final_title1 = str;
+				str_title = str;
 			}
 		}
-		final_str = final_str.replaceAll(".*src=\"","");
-		final_str = final_str.replaceAll("\".*","");
-    	final_title = final_title.replaceAll(".*.com/","");
-    	final_title = final_title.replaceAll("/.*","");
-    	final_title1 = final_title1.replaceAll(".*<title>", "");
-    	final_title1 = final_title1.replaceAll("xkcd: ", "");
-    	final_title1 = final_title1.replaceAll("</title>.*", "");
-		final_itext = final_itext.replaceAll(".*title=\"","");
-		final_itext = final_itext.replaceAll("\".*","");
-		strip.setTitle("Xkcd: " + final_title1 + " : " + final_title);
-		strip.setText(final_itext);
-		return final_str; 
+		str_index = str_index.replaceAll(".*.com/","");
+		str_index = str_index.replaceAll("/.*","");
+		str_title = str_title.replaceAll(".*xkcd: ", "");
+		str_title = str_title.replaceAll("</title>.*", "");
+		str_itext = str_itext.replaceAll(".*title=\"","");
+		str_itext = str_itext.replaceAll("\".*","");
+		strip.setTitle("Xkcd: " + str_index + ": " + str_title);
+		strip.setText(str_itext);
+		return image_url;
 	}
 }
